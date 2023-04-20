929a3f51f43d94a8e72c1f2b63054cd90928609e
mael098 <juanvillegasmorales@outlook.com>
2023-04-20 14:55:12 -0600

    commit

---
+++ app.java
@@ -0,0 +1,24 @@
+public class app {
+    private String numero;
+    private String name;
+    private String grupos;
+    private String carrera;
+    private double calificacion1;
+    private double calificacion2;
+    private double calificacion3;
+    private double calificacionFinal;
+    
+    public app(String numero, String name, String grupos, String carrera, double calificacion1, double calificacion2, double calificacion3, double calificasionFinal){ç
+    this.numero = numero;
+    this.name = name;
+    this.grupos = grupos;
+    this.carrera = carrera;
+    this.calificacion1 = calificacion1;
+    this.calificacion2 = calificacion2;
+    this.calificacion3 = calificacion3;
+    this.calificacionFinal = calificacionFinal;
+    }
+    
+
+
+}
